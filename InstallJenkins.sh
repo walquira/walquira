e63b4b6bc033688978f67342b01fe11872f70e45
walquira <31322142+walquira@users.noreply.github.com>
2019-10-11 14:58:04 -0300

    Update and rename README.md to InstallJenkins.sh

---
+++ InstallJenkins.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+sudo yum install java-1.8.0-openjdk-devel
+curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+sudo yum install jenkins
+sudo systemctl start jenkins
+systemctl status jenkins
+sudo systemctl enable jenkins
--- README.md
+++
@@ -1,2 +0,0 @@
-# walquira
-walquira
